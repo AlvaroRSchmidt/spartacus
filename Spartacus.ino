b56a485c60fb61db3f9e3188ae0466ee86b2587c
AlvaroRSchmidt <alvaror.schmidt@gmail.com>
2018-09-05 23:10:29 -0300

    simplificando logica que pisca o led

--- Spartacus.ino
+++ Spartacus.ino
@@ -2,21 +2,20 @@
 #include "SpartacusUtils.h"
 
 #define ledPin 8
-#define updateTime 1000
+#define updateTime 500
 
 float previousMillis, currentMillis; 
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   currentMillis = millis();
   if(currentMillis - previousMillis >= updateTime){
     previousMillis = currentMillis;
-    
-    tooglePin(ledPin);
-    delay(500);
-    tooglePin(ledPin);
+
+    togglePin(ledPin);
   }
 }
